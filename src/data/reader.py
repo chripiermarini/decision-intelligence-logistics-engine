b5e1a4fbf50d8cd0633fd0982c05f1811a5ebe23
chripiermarini <chripiermarini@gmail.com>
2026-04-15 10:11:49 +0200

    refactor(data): refactored Reader class

--- src/data/reader.py
+++ src/data/reader.py
@@ -1,41 +1,31 @@
-from data.input_data import InputData
 from pathlib import Path
 import polars as pl
 from polars import DataFrame
 
+from data.input_data import InputData
+
 
 class Reader:
+    def __init__(self, input_path: Path):
+        self.input_path = input_path
+        self._validate_path()
 
-    def __init__(self, input_file_path: str | Path):
-        self.input_file_path = Path(input_file_path)
-
-        if not self.input_file_path.exists(): ## ! CAREFUL !
-            raise ValueError(f"Path does not exist: {self.input_file_path}")
-
-    def read_input_files(self) -> InputData:
-        return InputData(
-            demand_history=self.read_demand_history_file(),
-            destinations=self.read_destinations_file(),
-            lanes=self.read_lanes_file(),
-            origins=self.read_origins_file(),
-        )
-
-    def read_demand_history_file(self) -> DataFrame:
-        return self._read_parquet("demand_history.parquet")
-
-    def read_destinations_file(self) -> DataFrame:
-        return self._read_parquet("destinations.parquet")
-
-    def read_lanes_file(self) -> DataFrame:
-        return self._read_parquet("lanes.parquet")
-
-    def read_origins_file(self) -> DataFrame:
-        return self._read_parquet("origins.parquet")
+    def _validate_path(self) -> None:
+        if not self.input_path.exists():
+            raise FileNotFoundError(f"Input path not found: {self.input_path}")
 
     def _read_parquet(self, filename: str) -> DataFrame:
-        file_path = self.input_file_path / filename
+        file_path = self.input_path / filename
 
         if not file_path.exists():
-            raise ValueError(f"File not found: {file_path}")
+            raise FileNotFoundError(f"Missing file: {file_path}")
 
         return pl.read_parquet(file_path)
+
+    def read(self) -> InputData:
+        return InputData(
+            demand_history=self._read_parquet("demand_history.parquet"),
+            destinations=self._read_parquet("destinations.parquet"),
+            lanes=self._read_parquet("lanes.parquet"),
+            origins=self._read_parquet("origins.parquet"),
+        )
