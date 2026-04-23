7a95dfb0f890742e90b555d06970456e1e1121fa
chripiermarini <chripiermarini@gmail.com>
2026-04-23 19:13:07 +0200

    test(scripts): generating second dataset and testing against different .yaml file

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 
-from data.generation import generate_synthetic_logistics_data
+from data.generation import (
+    generate_synthetic_logistics_data,
+)
 from datetime import datetime
 
 
-def main() -> None:
-    output_dir = Path("../data/synthetic")
+def first_synthetic_dataset() -> None:
+    output_dir = Path("../data/synthetic1")
     output_dir.mkdir(parents=True, exist_ok=True)
 
     generate_synthetic_logistics_data(
@@ -20,5 +22,19 @@ def main() -> None:
     print(f"Synthetic data generated in: {output_dir.resolve()}")
 
 
+def second_synthetic_dataset() -> None:
+    output_dir = Path("../data/synthetic2")
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    generate_synthetic_logistics_data(
+        output_dir=output_dir,
+        n_origins=3,
+        n_destinations=6,
+        start_date=datetime(2025, 1, 1),
+        end_date=datetime(2025, 6, 30),
+        seed=42,
+    )
+
+
 if __name__ == "__main__":
-    main()
+    second_synthetic_dataset()
